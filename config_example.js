beb5223b9c74e13144832d922c7c02148a4ab941
Blaze117 <51538244+Blaze117@users.noreply.github.com>
2019-06-30 21:32:15 -0500

    Add files via upload

---
+++ config_example.js
@@ -0,0 +1,8 @@
+var config = {
+  consumer_key:         '(CONSUMER KEY)',
+  consumer_secret:      '(CONSUMER SECRET KEY)',
+  access_token:         '(ACCESS TOKEN)',
+  access_token_secret:  '(SECRET ACCESS TOKEN)'
+}
+
+module.exports = config;
