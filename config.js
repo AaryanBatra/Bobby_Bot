3bf2b185e8d050a07fb91a9d9ef729ef58878945
Blaze117 <51538244+Blaze117@users.noreply.github.com>
2019-06-30 21:25:36 -0500

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-  consumer_key:         'UA8lF5VHXgp0WVMNLRCTGNE8D',
-  consumer_secret:      'gOpe09HdLjAqSS7pqnaHQGxV23e15ob0u8DYtXRC9SKx1tWs4Q',
-  access_token:         '1144355976679763982-6vv1U57MO62cn9Xq1xs6Nz7uDe4Lm4',
-  access_token_secret:  '7r5Ras58uFVy2xZ0YZ7t5O58mO8Nb7C2rb6TQ8tvdyrEU'
-}
-
-module.exports = config;
